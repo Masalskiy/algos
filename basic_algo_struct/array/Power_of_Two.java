186acf547749e31551adbb002f9e6224d121f76a
Denis Masalskii <masdv973@gmail.com>
2024-02-19 16:27:35 +0300

    good solve  cd /home/denis/Projects/algos ; /usr/bin/env /usr/lib/jvm/java-8-openjdk-amd64/bin/java -cp /home/denis/.config/Code/User/workspaceStorage/174bf3cf4f68c4f1dd9b5e70476e7909/redhat.java/jdt_ws/algos_f3c69bc4/bin Power_of_Two

--- basic_algo_struct/array/Power_of_Two.java
+++ basic_algo_struct/array/Power_of_Two.java
@@ -1,12 +1,11 @@
 //https://leetcode.com/problems/power-of-two/?envType=daily-question&envId=2024-02-19
 public class Power_of_Two {
     public static void main(String[] args) {
-        System.out.println(isPowerOfTwo(1));
+        System.out.println(isPowerOfTwo_v2(-2));
     }
 
+    // 16/2=8/2=4/2=2/2=1
     public static boolean isPowerOfTwo(int n) {
-        System.out.println(n%2);
-        
         if(n==1){
             return true;
         }
@@ -17,4 +16,24 @@ public class Power_of_Two {
             return isPowerOfTwo(n/2);
         }
     }
+    // при возведении в квадрат получаем только положительные числа
+    //16 ->  10000 16-1=15->01111
+    // 8  -> 01000 8-1=7 -> 00111 res 01000 & 00111 = 0
+    // 4  -> 00100 4-1=3 -> 00011
+    // 2  -> 00010 2-1=1 -> 00001
+    // 0  -> 00000 0-1=-1
+
+    //15 –> 01111 
+    //14 -> 01110
+    //res-> 01110
+    //
+    public static boolean isPowerOfTwo_v2(int n) {
+        if(n<=0){
+            return false;
+        }
+        else {
+            return (n & (n-1)) == 0;
+        }
+    }
+
 }
